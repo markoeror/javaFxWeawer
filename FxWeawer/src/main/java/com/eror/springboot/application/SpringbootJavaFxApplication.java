c4999c4778974283dcc2c44c110c7e6ee1b5a377
markoeror <erormarko.84@gmail.com>
2020-02-24 19:27:54 +0100

    start

---
+++ FxWeawer/src/main/java/com/eror/springboot/application/SpringbootJavaFxApplication.java
@@ -0,0 +1,43 @@
+package com.eror.springboot.application;
+
+import com.eror.springboot.JavafxWeaverSpringbootSampleApplication;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.stage.Stage;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+
+/**
+ * @author <a href="mailto:rene.gielen@gmail.com">Rene Gielen</a>
+ * @noinspection RedundantThrows
+ */
+public class SpringbootJavaFxApplication extends Application {
+
+    private ConfigurableApplicationContext context;
+
+    @Override
+    public void init() throws Exception {
+        ApplicationContextInitializer<GenericApplicationContext> initializer =
+                context -> {
+                    context.registerBean(Application.class, () -> SpringbootJavaFxApplication.this);
+                    context.registerBean(Parameters.class, this::getParameters); // for demonstration, not really needed
+                };
+        this.context = new SpringApplicationBuilder()
+                .sources(JavafxWeaverSpringbootSampleApplication.class)
+                .initializers(initializer)
+                .run(getParameters().getRaw().toArray(new String[0]));
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        context.publishEvent(new StageReadyEvent(primaryStage));
+    }
+
+    @Override
+    public void stop() throws Exception {
+        this.context.close();
+        Platform.exit();
+    }
+}
---
+++ FxWeawer/src/main/java/com/eror/springboot/controller/DialogController.java
@@ -0,0 +1,71 @@
+package com.eror.springboot.controller;
+
+import com.eror.springboot.JavafxWeaverSpringbootSampleApplication;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import net.rgielen.fxweaver.core.FxControllerAndView;
+import net.rgielen.fxweaver.core.FxWeaver;
+import net.rgielen.fxweaver.core.FxmlView;
+import org.springframework.beans.factory.InjectionPoint;
+import org.springframework.stereotype.Component;
+
+
+/**
+ * DialogController.
+ *
+ * @author Rene Gielen
+ * @noinspection WeakerAccess
+ */
+@Component
+@FxmlView("SimpleDialog.fxml")
+public class DialogController {
+
+    private Stage stage;
+
+    @FXML
+    private Label nameLabel ;
+
+    public void setName(String name) {
+        nameLabel.setText(name);
+    }
+    @FXML
+    private Button openAnotherDialogButton;
+    @FXML
+    private Button closeButton;
+    @FXML
+    private VBox dialog;
+
+    private final FxControllerAndView<AnotherDialog, VBox> anotherControllerAndView;
+
+    /**
+     * This injection is powered by
+     * {@link JavafxWeaverSpringbootSampleApplication#controllerAndView(FxWeaver, InjectionPoint)}
+     * <p/>
+     * Your IDE might get confused, but it works :)
+     */
+    public DialogController(FxControllerAndView<AnotherDialog, VBox> anotherControllerAndView) {
+        this.anotherControllerAndView = anotherControllerAndView;
+    }
+
+    @FXML
+    public void initialize() {
+        this.stage = new Stage();
+        stage.setScene(new Scene(dialog));
+
+        openAnotherDialogButton.setOnAction(
+                actionEvent -> anotherControllerAndView.getController().show()
+        );
+        closeButton.setOnAction(
+                actionEvent -> stage.close()
+        );
+    }
+
+    public void show() {
+        stage.show();
+    }
+
+}
