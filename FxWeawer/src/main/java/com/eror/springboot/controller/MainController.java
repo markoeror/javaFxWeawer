32d5a3fd8fe276e4f6e9f0caa91be74263753a1b
markoeror <erormarko.84@gmail.com>
2020-02-24 22:43:19 +0100

    fix 0.2

--- FxWeawer/src/main/java/com/eror/springboot/controller/MainController.java
+++ FxWeawer/src/main/java/com/eror/springboot/controller/MainController.java
@@ -1,12 +1,16 @@
 package com.eror.springboot.controller;
 
+import com.eror.springboot.service.UserService;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import net.rgielen.fxweaver.core.FxControllerAndView;
 import net.rgielen.fxweaver.core.FxWeaver;
 import net.rgielen.fxweaver.core.FxmlView;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -17,6 +21,21 @@ public class MainController {
     private final String greeting;
     private final FxWeaver fxWeaver;
     private final FxControllerAndView<DialogController, VBox> dialog;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private Label lblLogin;
+
+    @Autowired
+    private UserService userService;
 
     @FXML
     public Label label;
@@ -35,6 +54,16 @@ public class MainController {
         this.dialog = dialog;
     }
 
+
+
+    public String getPassword() {
+        return password.getText();
+    }
+
+    public String getUsername() {
+        return username.getText();
+    }
+
     @FXML
     public void initialize() {
         helloButton.setOnAction(
@@ -46,13 +75,34 @@ public class MainController {
         );
 */
         openSimpleDialogButton.setOnAction(
-                actionEvent ->{
+                actionEvent -> {
                     dialog.getController().setName("Marko Eror");
                     dialog.getController().show();
                 }
 
         );
+        loginButton.setOnAction(
+                actionEvent -> {
+                    if(userService.authenticate(getUsername(), getPassword())){
 
+                        FxControllerAndView<TiledDialogController, VBox> tiledDialog =
+                                fxWeaver.load(TiledDialogController.class);
+                        tiledDialog.getView().ifPresent(
+                                v -> {
+                                    Label label = new Label();
+                                    label.setText("Dynamically added Label");
+                                    v.getChildren().add(label);
+                                }
+                        );
+                        tiledDialog.getController().show();
+
+
+                    }else{
+                        lblLogin.setText("Login Failed.");
+                    }
+
+                }
+        );
         openTiledDialogButton.setOnAction(
                 actionEvent -> {
                     FxControllerAndView<TiledDialogController, VBox> tiledDialog =
